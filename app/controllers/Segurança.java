34b4b93c0d2eb0fb74078bcd2524dba9fe7efb13
Nayara Oliveira <nyolives7@gmail.com>
2025-10-16 08:36:52 -0300

    Autenticação de atendente

--- app/controllers/Segurança.java
+++ app/controllers/Segurança.java
@@ -8,22 +8,13 @@ import security.Atendente;
 public class Segurança extends Controller {
 
 	@Before
-	static void verificacao() {
-		if(session.get("cargo") == null) {
-			flash.error("Você deve estar logado para acessar esta página");
-			Logins.login();
-		}
-		
-	}
-	
-	@Before
 	static void verificarAtendente() {
-		String cargo = session.get("userCargo");
+		String cargo = session.get("cargo");
 		Atendente annotation = getActionAnnotation(Atendente.class);
-		if(annotation != null && session.get("cargo") != "ATENDENTE") {
+		flash.success(cargo);
+		if(annotation != null && (cargo == null || !cargo.equals("ATENDENTE"))) {
 			flash.error("Você deve estar logado como Atendente para acessar esta página");
 			Logins.login();
-		}
-		
+		}	
 	}
 }
